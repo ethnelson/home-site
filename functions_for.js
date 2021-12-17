ef8a1217d3f55abb3e1402420c224e56f7b9260d
Ethan Nelson <eth9923@gmail.com>
2021-12-16 23:33:08 -0600

    adding unit testing capability to backend

---
+++ functions_for.js
@@ -0,0 +1,8 @@
+// This file is for making functions to test
+
+function divide(a, b) {
+    return a / b;
+}
+
+module.exports = divide;
+
---
+++ functions_for.test.js
@@ -0,0 +1,5 @@
+const divide = require("./functions_for");
+
+test('Divides 6 by 3 to equal 2', () => {
+    expect(divide(6, 3)).toBe(2);
+});
