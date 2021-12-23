d77114af734f97cd205427402cceac952d433ad3
Ethan Nelson <eth9923@gmail.com>
2021-12-23 14:28:04 -0600

    Added css for ProjectArchive page

--- client/src/ProjectArchive/ProjectArchive.js
+++ client/src/ProjectArchive/ProjectArchive.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 
+import './ProjectArchive.css';
 import logo from '../logo.svg';
 
 // Projects Archive Page ---------------------------------------------------- //
 class ProjectArchive extends Component {
     render() {
         return (
-            <ProjectSnippet
-                projectIcon={logo}
-                name="game Game"
-                description="Something something something description of this
-                    super cool game thing"/>
+            <div className="project-archive">
+                <ProjectSnippet
+                    projectIcon={logo}
+                    name="HDIGH"
+                    description="How did I get here?"/>
+                <ProjectSnippet
+                    projectIcon={logo}
+                    name="Weird Tool"
+                    description="Ever wonder what a weird tool looks like?"/>
+                <ProjectSnippet
+                    projectIcon={logo}
+                    name="Game 2"
+                    description="OOooo a game, what a mystery."/>
+                <ProjectSnippet
+                    projectIcon={logo}
+                    name="Sound generator"
+                    description="This makes some weird sounds, or whatever you
+                    want really."/>
+            </div>
         );
     }
 }
@@ -21,7 +36,7 @@ class ProjectSnippet extends Component {
             <div className="project-snippet">
                 <img src={this.props.projectIcon} className="project-icon" alt="project" />
                 <span className="project-details">
-                    <p className="project-name">{this.props.name}</p>
+                    <h3 className="project-name">{this.props.name}</h3>
                     <p className="project-desc">{this.props.description}</p>
                 </span>
             </div>
